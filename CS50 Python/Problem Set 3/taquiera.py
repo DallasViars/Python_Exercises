bd970c37a0911ebc38dd8a24bea59e56721c0148
Dallas Viars <97245426+DallasViars@users.noreply.github.com>
2022-05-02 10:28:53 -0400

    Turned in work

---
+++ CS50 Python/Problem Set 3/taquiera.py
@@ -0,0 +1,75 @@
+    # Felipe's Taqueria
+    # One of the most popular places to eat in Harvard Square is Felipe’s Taqueria, which offers a menu of entrees, per the dict below, wherein the value of each key is a price in dollars:
+
+    # {
+    #     "Baja Taco": 4.00,
+    #     "Burrito": 7.50,
+    #     "Bowl": 8.50,
+    #     "Nachos": 11.00,
+    #     "Quesadilla": 8.50,
+    #     "Super Burrito": 8.50,
+    #     "Super Quesadilla": 9.50,
+    #     "Taco": 3.00,
+    #     "Tortilla Salad": 8.00
+    # }
+    # In a file called taqueria.py, implement a program that enables a user to place an order, prompting them for items, one per line, until the user inputs control-d (which is a common way of ending one’s input to a program). After each inputted item, display the total cost of all items inputted thus far, prefixed with a dollar sign ($) and formatted to two decimal places. Treat the user’s input case insensitively. Ignore any input that isn’t an item. Assume that every item on the menu will be titlecased.
+
+    # Hints
+    # Note that you can detect when the user has inputted control-d by catching an EOFError with code like:
+    # try:
+    #     item = input()
+    # except EOFError:
+    #     ...
+    # You might want to print a new line so that the user’s cursor (and subsequent prompt) doesn’t remain on the same line as your program’s own prompt.
+
+    # Inputting control-d does not require inputting Enter as well, and so the user’s cursor (and subsequent prompt) might thus remain on the same line as your program’s own prompt. You can move the user’s cursor to a new line by printing \n yourself!
+    # Note that a dict comes with quite a few methods, per docs.python.org/3/library/stdtypes.html#mapping-types-dict, among them get, and supports operations like:
+    # d[key]
+    # and
+
+    # if key in d:
+    #     ...
+    # wherein d is a dict and key is a str.
+
+    # Be sure to avoid or catch any KeyError.
+
+
+
+def main():
+    tacos()
+
+menu = {
+    "Baja Taco": 4.00,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
+
+def tacos(total=0, order=""):
+
+    try:
+        item = input(f"Item: ").title()
+    except EOFError:
+
+        #When pressing Control+D ends program. Prints new line to input, shows user's order and total on different lines
+        print(f"", end="\n")
+        print(f"Your order: {order[0:len(order)-2]}", end="\n")
+        print(f"Total: ${total:.2f}", end="\n")
+        return
+
+    #finds the item on the menu and adds its price to the menu, if it isn't found reprompts user
+    if item in menu:
+        total += menu[item]
+        print(f"Total: ${total:.2f}")
+        order += item + ", "
+        tacos(total, order)
+    else:
+        tacos(total, order)
+
+
+main()
